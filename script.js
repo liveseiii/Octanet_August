d1fb3f1afbba8ac2989176e344a28c0d22f9d78a
liveseiii <136844460+liveseiii@users.noreply.github.com>
2024-08-24 15:59:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Thank you for your interest! Check your email for more resources.');
+});
+
+document.getElementById('cta-button-footer').addEventListener('click', function() {
+    alert('Thank you for signing up! We will send you free resources.');
+});
